dad4d24888d987b6f8efd987e3c043693b9f956c
AS <aleksander@uixlimited.com>
2019-05-04 23:25:48 +0100

    install css loader

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from 'react'
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
-import Stepper from '../src';
+import Stepper from '../src'
 
-storiesOf('Button', module)
-  .add('with text', () => <Stepper onClick={action('clicked')}></Stepper>)
+storiesOf('Wrapup', module).add('with steps', () => (
+    <Stepper activeStep={1}>
+        <>Step 1</>
+        <>Step 2</>
+        <>Step 3</>
+    </Stepper>
+))
