b6140e95a1fe0ad016e2f88572c5a570131b6877
AS <aleksander@uixlimited.com>
2019-05-05 21:21:19 +0100

    finish styling

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,28 +1,23 @@
 import React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import { action } from '@storybook/addon-actions'
 
 import Stepper from '../src'
 
+const styles = {
+    width: 400,
+}
+
 storiesOf('Wrapup', module).add('with scroll', () => (
-    <Stepper activeStep={1}>
-        <>Personal details</>
-        <>Contact details</>
-        <>Billing</>
-        <>Step 4</>
-        <>Step 5</>
-        <>Step 6</>
-        <>Step 7</>
-        <>Step 8</>
-        <>Step 9</>
-        <>Step 10</>
-        <>Step 11</>
-        <>Step 12</>
-        <>Step 13</>
-        <>Step 14</>
-        <>Step 15</>
-        <>Step 16</>
-        <>Step 17</>
-    </Stepper>
+    <div style={styles}>
+        <Stepper activeStep={1}>
+            <>Personal details</>
+            <>Contact details</>
+            <>Prepare</>
+            <>Select your IELTS</>
+            <>Register</>
+            <>Secure your place</>
+            <>Confirmation</>
+        </Stepper>
+    </div>
 ))
